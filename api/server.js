3831a8b8dafe8596b42b156e55cecb34a46003d7
wreck888 <DChui88@gmail.com>
2022-02-11 16:43:09 -0600

    mvp v1

--- api/server.js
+++ api/server.js
@@ -8,8 +8,4 @@ server.use(express.json());
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
 
-server.use('*', (req, res) => {
-    res.send('<h1> Hello, Heroku! </h1>')
-})
-
 module.exports = server;
